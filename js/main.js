160ea526d457358f20f93a21f5d84f42a22fedb3
Guillermo Fuentes <guille@iMac-de-Guillermo.local>
2026-04-12 22:48:41 -0600

    degradado en titulo

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@
                 onComplete: () => console.log("Animación completada")
             }, "-=.85")
             .from(titulo, {
-                y: window.innerWidth < 768 ? 40 : 90,
+                y: window.innerWidth < 768 ? 60 : 90,
                 duration: 1,
                 ease: "power2.Out",
                 stagger: 0, // Pequeño retraso entre animaciones
