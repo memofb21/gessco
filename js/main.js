724877d8e3f0d022042101915a5fbe0c0ae31f46
Guillermo Fuentes <guille@iMac-de-Guillermo.local>
2026-04-16 02:03:31 -0600

    pre-final5

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@
             scrollTrigger:{
                 trigger:"#home",
                 start:"top top",
-                end: esTelefono ? "+=20%" : "+=120%",
+                end: esTelefono ? "+=5%" : "+=120%",
                 scrub:1,
                 pin: esTelefono ? false : true,  // 👈 AQUÍ EL CAMBIO
                 anticipatePin:1,
